db0afae3c7397a889395d141a7f2fed8ca93efcb
ojashwisengar <ojashwi.sengar@gmail.com>
2026-01-23 23:34:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import pandas as pd
+import joblib
 
+model = joblib.load("churn_model.pkl")
+features = joblib.load("model_features.pkl")
+
+st.title("Customer Churn Prediction")
+st.write("Enter Customer details to predict churn risk")
+
+input_data = {}
+
+for f in features:
+  input_data[f] = st.number_input(f, value = 0.0)
+
+input_df = pd.DataFrame([input_data])
+
+if st.button("Predict"):
+  prob = model.predict_proba(input_df)[0][1]
+  st.subheader(f"Churn Probability: {prob: .2f}")
+  if prob >= 0.4:
+    st.error("High risk of churn")
+  else:
+    st.success("Low risk of churn")
